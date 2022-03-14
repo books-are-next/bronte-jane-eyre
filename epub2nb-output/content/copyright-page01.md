c4575b000820eba8d9526c6089010f4a9fb696e0
gabGodiskova <gabgodiskova@gmail.com>
2022-03-14 16:06:22 +0000

    Deploying to docs from @ books-are-next/bronte-jane-eyre@d415345ec7099269b6e22cca542f7ee2a4e7e4ed 🚀

---
+++ epub2nb-output/content/copyright-page01.md
@@ -0,0 +1,18 @@
+---
+title: ''
+contentType: prose
+---
+
+# Jane Eyre, an Autobiography
+
+by Charlotte Brontë
+
+Rights for this book: [CC BY-NC](http://creativecommons.org/licenses/by-nc/4.0/)
+
+This edition is published by Recovering the Classics.
+
+Originally [issued by Project Gutenberg](http://www.gutenberg.org/ebooks/1260) on 1998-03-01. To support the work of Project Gutenberg, visit their [Donation Page](https://www.gutenberg.org/wiki/Gutenberg:Project_Gutenberg_Needs_Your_Donation).
+
+This free ebook has been produced by [GITenberg](https://www.GITenberg.org/), a program of the [Free Ebook Foundation](http://ebookfoundation.org/). If you have corrections or improvements to make to this ebook, or you want to use the source files for this ebook, visit [the book's github repository](https://github.com/GITenberg/Jane-Eyre_1260/). You can support the work of the Free Ebook Foundation at their [Contributors Page.](http://ebookfoundation.org/contributions.html)
+
+The original art is [Attribution-NonCommercial 4.0 International (CC BY-NC 4.0)](https://creativecommons.org/licenses/by-nc/4.0/), by Ed Gaither - Modern Electrographic, 2015.
---
+++ service-worker.js
@@ -0,0 +1,60 @@
+/* eslint-disable no-restricted-globals */
+
+/* global self, caches, fetch */
+
+const CACHE = 'cache-d415345';
+
+self.addEventListener('install', e => {
+  e.waitUntil(precache()).then(() => self.skipWaiting());
+});
+
+self.addEventListener('activate', event => {
+  self.clients
+    .matchAll({
+      includeUncontrolled: true,
+    })
+    .then(clientList => {
+      const urls = clientList.map(client => client.url);
+      console.log('[ServiceWorker] Matching clients:', urls.join(', '));
+    });
+
+  event.waitUntil(
+    caches
+      .keys()
+      .then(cacheNames =>
+        Promise.all(
+          cacheNames.map(cacheName => {
+            if (cacheName !== CACHE) {
+              console.log('[ServiceWorker] Deleting old cache:', cacheName);
+              return caches.delete(cacheName);
+            }
+            return null;
+          })
+        )
+      )
+      .then(() => {
+        console.log('[ServiceWorker] Claiming clients for version', CACHE);
+        return self.clients.claim();
+      })
+  );
+});
+
+function precache() {
+  return caches.open(CACHE).then(cache => cache.addAll(["./","./ch01.html","./ch02.html","./ch03.html","./ch04.html","./ch05.html","./ch06.html","./ch07.html","./ch08.html","./ch09.html","./ch10.html","./ch11.html","./ch12.html","./ch13.html","./ch14.html","./ch15.html","./ch16.html","./ch17.html","./ch18.html","./ch19.html","./ch20.html","./ch21.html","./ch22.html","./ch23.html","./ch24.html","./ch25.html","./ch26.html","./ch27.html","./ch28.html","./ch29.html","./ch30.html","./ch31.html","./ch32.html","./ch33.html","./ch34.html","./ch35.html","./ch36.html","./ch37.html","./ch38.html","./ch39.html","./ch40.html","./colophon.html","./copyright-page01.html","./cover.html","./favicon.png","./index.html","./manifest.json","./titlepage01.html","./fonts/Literata-Italic-var.woff2","./fonts/Literata-var.woff2","./fonts/LiterataTT-TextItalic.woff2","./fonts/LiterataTT-TextRegular.woff2","./fonts/LiterataTT-TextSemibold.woff2","./fonts/LiterataTT_LICENSE.txt","./fonts/SpaceGroteskVF.woff2","./fonts/SpaceGroteskVF_LICENSE.txt","./resources/cover.jpg","./resources/p107s.jpg","./resources/p140s.jpg","./resources/p184s.jpg","./resources/p190s.jpg","./resources/p272s.jpg","./resources/p30s.jpg","./resources/p311s.jpg","./resources/p316s.jpg","./resources/p323s.jpg","./resources/p369s.jpg","./resources/p389s.jpg","./resources/p413s.jpg","./resources/p422s.jpg","./resources/p435s.jpg","./scripts/bundle.js","./style/style.min.css","./template-images/circles.png"]));
+}
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.open(CACHE).then(cache => {
+      return cache.match(e.request).then(matching => {
+        if (matching) {
+          console.log('[ServiceWorker] Serving file from cache.');
+          console.log(e.request);
+          return matching;
+        }
+
+        return fetch(e.request);
+      });
+    })
+  );
+});
